ac37ecdd21a812f67c76178029175390aa147097
Ralle1968 <graef.ralf@gmail.com>
2017-04-24 14:37:24 +0200

    first commit

---
+++ 1_einfuehrung/loesung.php
@@ -0,0 +1,16 @@
+<?php
+
+class Praktikum {
+    public $name = "";
+    public $geburtsDatum = "";
+    public $praktikumsDauer = 0;
+    public $betreuendePerson = "";
+    public $ansprechpartner = "";
+
+}
+
+$praktikant1 = new Praktikum();
+$praktikant1->name = "Ralf Gräf";
+$praktikant1->geburtsDatum = "14.04.1968";
+$praktikant1->praktikumsDauer = 21;
+$praktikant1->ansprechpartner = "Ralf";
---
+++ 2_methoden/Methoden.php
@@ -0,0 +1,11 @@
+<?php
+
+class Methoden {
+    function addieren() {
+        echo "4 + 6 = " . (4 + 6);
+    }
+}
+$objekt = new Methoden();
+$objekt->addieren();
+$objekt2 = new Methoden();
+$objekt2->addieren();
